5be81997239ff620ba1fe0a68501d7e78940207b
Taylor Carroll <taylorcarroll@Taylors-MacBook-Pro.local>
2019-08-27 16:27:53 -0500

    pushing up questionmark branch

--- src/script/main.js
+++ src/script/main.js
@@ -6,19 +6,24 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement (theWordArray, punctuation) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
-
+    
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp = buildMeUp + theWordArray[i] + " ";
+        if ((i + 1) % 3 === 0) { //condition for if it is divisible by 3
+            buildMeUp = buildMeUp + theWordArray[i] + `${punctuation} `; // Concatenate the new word onto buildMeUp
+            console.log(buildMeUp); //log to console
+        } else buildMeUp = buildMeUp + theWordArray[i] + " "; //else statement that adds a space
+            console.log(buildMeUp); //log to console
+
+            
+        }
         console.log(buildMeUp);
         // Print buildMeUp to the console
     }
 
-}
+// // Invoke the function and pass in the array
+addExcitement(sentence, "?")
 
-// Invoke the function and pass in the array
-addExcitement(sentence)
